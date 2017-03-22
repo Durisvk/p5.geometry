f85619ce12a2d813a38f9e6539d35af9c5108ee0
Juraj Carnogursky <juraj.carnogursky@student.tuke.sk>
2017-03-22 10:42:51 +0100

    adding find x-est vector and rotation2D 3D.

--- src/VectorList.js
+++ src/VectorList.js
@@ -94,6 +94,36 @@ p5.VectorList.prototype.get = function(index) {
 };
 
 /**
+ * Removes the vector from the VectorList.
+ * @param {p5.Vector} item - vector to be removed
+ */
+p5.VectorList.prototype.remove = function(item) {
+    var index = this.array.indexOf(item);
+    if(index > -1) {
+        this.splice(index, 1);
+    }
+    else {
+        throw new Error("Vector not found in list.");
+    }
+};
+
+/**
+ * Splices the item out of VectorList.
+ * @param {Number} index - if negative then found from the end
+ */
+p5.VectorList.prototype.splice = function(index, n) {
+    if(index < 0 && this.array.length + index > 0) {
+        this.array.splice(this.array.length + index, n);
+    }
+    else if(index > 0 && index < this.array.length) {
+        this.array.splice(index, n);
+    }
+    else {
+        throw new Error("Invalid index (out of bounds).");
+    }
+}
+
+/**
  * Gets all of the vectors in vector list.
  * @return {p5.Vector[]}
  */
@@ -174,6 +204,11 @@ p5.VectorList.prototype.geometricMean = function() {
     }
 };
 
+
+/**
+ * Finds a middle vector and returns it
+ * @return {p5.Vector} - the original vector
+ */
 p5.VectorList.prototype.findMiddle = function() {
     var avg = this.average();
     var minVec = null;
@@ -189,6 +224,183 @@ p5.VectorList.prototype.findMiddle = function() {
     }
     return minVec;
 };
+
+
+/**
+ * Returns the vector which is located most left.
+ * @return {p5.Vector} - the most left vector
+ */
+p5.VectorList.prototype.findLeftest = function() {
+    var minX = Infinity;
+    var minVec = null;
+    for ( var i = 0; i < this.array.length; i++ ) {
+        if(this.array[i].x < minX) {
+            minVec = this.array[i];
+            minX = this.array[i].x;
+        }
+    }
+    return minVec;
+};
+
+
+/**
+ * Returns the vector which is located most right.
+ * @return {p5.Vector} - the most right vector
+ */
+p5.VectorList.prototype.findRightest = function() {
+    var maxX = -Infinity;
+    var maxVec = null;
+    for ( var i = 0; i < this.array.length; i++ ) {
+        if(this.array[i].x > maxX) {
+            maxVec = this.array[i];
+            maxX = this.array[i].x;
+        }
+    }
+    return maxVec;
+};
+
+
+/**
+ * Returns the vector which is located most top.
+ * @return {p5.Vector} - the most top vector
+ */
+p5.VectorList.prototype.findHighest = function() {
+    var minY = Infinity;
+    var minVec = null;
+    for ( var i = 0; i < this.array.length; i++ ) {
+        if(this.array[i].y < minY) {
+            minVec = this.array[i];
+            minY = this.array[i].y;
+        }
+    }
+    return minVec;
+};
+
+/**
+ * Returns the vector which is located most bottom.
+ * @return {p5.Vector} - the most bottom vector
+ */
+p5.VectorList.prototype.findLowest = function() {
+    var maxY = -Infinity;
+    var maxVec = null;
+    for ( var i = 0; i < this.array.length; i++ ) {
+        if(this.array[i].y > maxY) {
+            maxVec = this.array[i];
+            maxY = this.array[i].y;
+        }
+    }
+    return maxVec;
+};
+
+/**
+ * Returns the vector which is the longest.
+ * @return {p5.Vector} - the most bottom vector
+ */
+p5.VectorList.prototype.findLongest = function() {
+    var maxLong = -Infinity;
+    var maxVec = null;
+    for ( var i = 0; i < this.array.length; i++ ) {
+        var long = Math.sqrt(Math.pow(this.array[i].x, 2) +
+                             Math.pow(this.array[i].y, 2) +
+                             Math.pow(this.array[i].x, 2));
+        if(long > maxLong) {
+            maxVec = this.array[i];
+            maxLong = long;
+        }
+    }
+    return maxVec;
+};
+
+/**
+ * Returns the vector which is the shortest.
+ * @return {p5.Vector} - the most bottom vector
+ */
+p5.VectorList.prototype.findShortest = function() {
+    var minLong = Infinity;
+    var minVec = null;
+    for ( var i = 0; i < this.array.length; i++ ) {
+        var long = Math.sqrt(Math.pow(this.array[i].x, 2) +
+                             Math.pow(this.array[i].y, 2) +
+                             Math.pow(this.array[i].z, 2));
+        if(long < minLong) {
+            minVec = this.array[i];
+            minLong = long;
+        }
+    }
+    return minVec;
+};
+
+/**
+ * Rotates all Vectors in the list by angle
+ * @param {number} angle - angle which the rotation is applied (in radians)
+ * @param {object|number} centerX - either object containing x and y or x coordinate of center of the rotation
+ * @param {undefined|number} centerY - y coordinate of center of the rotation
+ */
+p5.VectorList.prototype.rotate2D = function(angle, centerX, centerY) {
+    var center = {};
+    if ( typeof centerX === 'object' && centerX.hasOwnProperty('x') && centerX.hasOwnProperty('y') ) {
+        center.x = centerX.x;
+        center.y = centerX.y;
+    }
+    else if ( typeof centerX === 'undefined' && typeof centerY === 'undefined' ) {
+        center.x = 0;
+        center.y = 0;
+    }
+    else {
+        center.x = centerX;
+        center.y = centerY;
+    }
+
+    var sinAngle = Math.sin(angle);
+    var cosAngle = Math.cos(angle);
+
+    for( var i = 0; i < this.array.length; i++ ) {
+        var r = Math.sqrt(Math.pow(this.array[i].x - center.x, 2) + Math.pow(this.array[i].y - center.y, 2));
+        this.array[i].x = r * cosAngle;
+        this.array[i].y = r * sinAngle;
+    }
+};
+/**
+ * Rotates all Vectors in the list by angle in 3D
+ * @param {number} phi - angle which the rotation is applied of XY plane (in radians)
+ * @param {number} theta - angle which the rotation is applied of Z axis (in radians)
+ * @param {object|number} centerX - either object containing x, y and z or x coordinate of center of the rotation
+ * @param {undefined|number} centerY - y coordinate of center of the rotation
+ * @param {undefined|number} centerZ - z coordinate of center of the rotation
+ */
+p5.VectorList.prototype.rotate = function(phi, theta, centerX, centerY, centerZ) {
+    var center = {};
+    if ( typeof centerX === 'object' && centerX.hasOwnProperty('x') && centerX.hasOwnProperty('y') && centerX.hasOwnProperty('z') ) {
+        center.x = centerX.x;
+        center.y = centerX.y;
+        center.z = centerX.z;
+    }
+    else if ( typeof centerX === 'undefined' && typeof centerY === 'undefined' ) {
+        center.x = 0;
+        center.y = 0;
+        center.z = 0;
+    }
+    else {
+        center.x = centerX;
+        center.y = centerY;
+        center.z = centerZ;
+    }
+
+    var sinPhi = Math.sin(phi);
+    var cosPhi = Math.cos(phi);
+    var sinTheta = Math.sin(theta);
+    var cosTheta = Math.cos(theta);
+
+    for( var i = 0; i < this.array.length; i++ ) {
+        var r = Math.sqrt(Math.pow(this.array[i].x - center.x, 2) +
+                            Math.pow(this.array[i].y - center.y, 2) +
+                            Math.pow(this.array[i].z - center.z, 2));
+        this.array[i].x = r * cosPhi * sinTheta;
+        this.array[i].y = r * sinPhi * sinTheta;
+        this.array[i].z = r * cosTheta;
+    }
+};
+
 
 function add(v1, v2) {
     v1.x += v2.x;
--- test/VectorList.js
+++ test/VectorList.js
@@ -10,19 +10,19 @@ describe('p5.VectorList', function() {
     });
     it('should add vectors with constructor as separated arguments', function() {
         vl = new p5.VectorList({x: 5, y: 0}, {x: -1, y: -3});
-        expect(vl.array).to.have.lengthOf(2);
+        assert.lengthOf(vl.array, 2);
     });
     it('should add vectors with constructor as array', function() {
         vl = new p5.VectorList([{x: 5, y: 0}, {x: -1, y: -3}]);
-        expect(vl.array).to.have.lengthOf(2);
+        assert.lengthOf(vl.array, 2);
     });
     it('should add(vector), get(index), getAll()', function() {
         var vector1 = {x: 0, y: 5};
         var vector2 = {x: -3, y: 2};
         vl.add(vector1);
-        expect(vl.get(0)).to.equal(vector1);
+        assert.equal(vl.get(0), vector1);
         vl.add(vector2);
-        expect(vl.getAll()).to.have.lengthOf(2);
+        assert.lengthOf(vl.getAll(), 2);
     });
     it('should add multiple vectors at once as separated arguments', function() {
         var vector1 = {x: 1, y: 9};
@@ -30,7 +30,7 @@ describe('p5.VectorList', function() {
         var vector3 = {x: 8, y: -5};
 
         vl.add(vector1, vector2, vector3);
-        expect(vl.array).to.have.lengthOf(3);
+        assert.lengthOf(vl.array, 3);
     });
     it('should add multiple vectors at once as array', function() {
         var vector1 = {x: 1, y: 9};
@@ -38,30 +38,42 @@ describe('p5.VectorList', function() {
         var vector3 = {x: 8, y: -5};
 
         vl.add([vector1, vector2, vector3]);
-        expect(vl.array).to.have.lengthOf(3);
+        assert.lengthOf(vl.array, 3);
     });
+    it('should add, remove and splice the vectors from list', function() {
+        var vector1 = {x: 3, y: -1};
+        var vector2 = {x: 1, y: 2};
+        var vector3 = {x: 2, y: -3};
+
+        vl.add(vector1, vector2, vector3);
+        assert.lengthOf(vl.array, 3);
+        vl.remove(vector3);
+        assert.lengthOf(vl.array, 2);
+        vl.splice(-1, 1);
+        assert.lengthOf(vl.array, 1);     
+    })
     it('should chain add vectors', function() {
         var position = {x: 4, y: 5};
         var velocity = {x: 2, y: 1};
         var acceleration = {x: 1, y: -1};
         vl.add(position, velocity, acceleration);
         vl.chainAdd();
-        expect(vl.array[0].x).to.equal(6);
-        expect(vl.array[0].y).to.equal(6);
+        assert.equal(vl.array[0].x, 6);
+        assert.equal(vl.array[0].y, 6);
 
-        expect(vl.array[1].x).to.equal(3);
-        expect(vl.array[1].y).to.equal(0);
+        assert.equal(vl.array[1].x, 3);
+        assert.equal(vl.array[1].y, 0);
 
-        expect(vl.array[2].x).to.equal(1);
-        expect(vl.array[2].y).to.equal(-1);
+        assert.equal(vl.array[2].x, 1);
+        assert.equal(vl.array[2].y, -1);
     });
     it('should take the average of vectors', function() {
         var v1 = {x: 2, y: 4};
         var v2 = {x: 4, y: 2};
         vl.add(v1, v2);
         var average = vl.average();
-        expect(average.x).to.equal(3);
-        expect(average.y).to.equal(3);
+        assert.equal(average.x, 3);
+        assert.equal(average.y, 3);
     });
     it('should take geometric mean', function() {
         var v1 = {x: 2, y: 3};
@@ -80,5 +92,131 @@ describe('p5.VectorList', function() {
         vl.add(v1, v2, v3, v4);
         var mid = vl.findMiddle();
         assert.deepEqual(mid, v1);
+    });
+    it('should find a leftest point', function() {
+        var v1 = {x: 2, y: 3};
+        var v2 = {x: -1, y: 2};
+        var v3 = {x: 2, y: 4};
+        var v4 = {x: 4, y: 2};
+        vl.add(v1, v2, v3, v4);
+        var l = vl.findLeftest();
+        assert.equal(l.x, v2.x);
+        assert.equal(l.y, v2.y);
+    });
+    it('should find a rightest point', function() {
+        var v1 = {x: 2, y: 3};
+        var v2 = {x: -1, y: 2};
+        var v3 = {x: 2, y: 4};
+        var v4 = {x: 4, y: 2};
+        vl.add(v1, v2, v3, v4);
+        var r = vl.findRightest();
+        assert.equal(r.x, v4.x);
+        assert.equal(r.y, v4.y);
+    });
+    it('should find a highest point', function() {
+        var v1 = {x: 2, y: 3};
+        var v2 = {x: -1, y: 2};
+        var v3 = {x: 2, y: 4};
+        var v4 = {x: 4, y: 2};
+        vl.add(v1, v2, v3, v4);
+        var t = vl.findHighest();
+        assert.equal(t.x, v2.x);
+        assert.equal(t.y, v2.y);
+    });
+    it('should find a lowest point', function() {
+        var v1 = {x: 2, y: 3};
+        var v2 = {x: -1, y: 2};
+        var v3 = {x: 2, y: 4};
+        var v4 = {x: 4, y: 2};
+        vl.add(v1, v2, v3, v4);
+        var l = vl.findLowest();
+        assert.equal(l.x, v3.x);
+        assert.equal(l.y, v3.y);
+    });
+    it('should find a longest point', function() {
+        var v1 = {x: 2, y: 3};
+        var v2 = {x: -1, y: 2};
+        var v3 = {x: 2, y: 4};
+        var v4 = {x: 7, y: 2};
+        vl.add(v1, v2, v3, v4);
+        var l = vl.findLongest();
+        assert.equal(l.x, v4.x);
+        assert.equal(l.y, v4.y);
+    });
+    it('should find a shortest point', function() {
+        var v1 = {x: 2, y: 3};
+        var v2 = {x: -1, y: 2};
+        var v3 = {x: 2, y: 4};
+        var v4 = {x: 7, y: 2};
+        vl.add(v1, v2, v3, v4);
+        var l = vl.findShortest();
+        assert.equal(l.x, v2.x);
+        assert.equal(l.y, v2.y);
+    });
+    it('should rotate in 2D when given center vector', function() {
+        var v1 = {x: 2, y: 3};
+        var v2 = {x: -1, y: 2};
+        vl.add(v1, v2);
+        vl.rotate2D(Math.PI/3, {x: 3, y: 3});
+        assert.closeTo(v1.x, 0.5, 0.001);
+        assert.closeTo(v1.y, 0.8660, 0.001);
+        assert.closeTo(v2.x, 2.06155, 0.001);
+        assert.closeTo(v2.y, 3.5707, 0.001);
+    });
+    it('should rotate in 2D when given centerX and centerY', function() {
+        var v1 = {x: 2, y: 3};
+        var v2 = {x: -1, y: 2};
+        vl.add(v1, v2);
+        vl.rotate2D(Math.PI/3, 3, 3);
+        assert.closeTo(v1.x, 0.5, 0.001);
+        assert.closeTo(v1.y, 0.8660, 0.001);
+        assert.closeTo(v2.x, 2.06155, 0.001);
+        assert.closeTo(v2.y, 3.5707, 0.001);
+    });
+    it('should rotate in 2D when given no center', function() {
+        var v1 = {x: 2, y: 3};
+        var v2 = {x: -1, y: 2};
+        vl.add(v1, v2);
+        vl.rotate2D(Math.PI/3);
+        assert.closeTo(v1.x, 1.8027, 0.001);
+        assert.closeTo(v1.y, 3.1224, 0.001);
+        assert.closeTo(v2.x, 1.1180, 0.001);
+        assert.closeTo(v2.y, 1.9364, 0.001);
+    });
+    it('should rotate in 3D when given center vector', function() {
+        var v1 = {x: 2, y: 3, z: 1};
+        var v2 = {x: -1, y: 2, z: 4};
+        vl.add(v1, v2);
+        vl.rotate(Math.PI/3, Math.PI/4, {x: 2, y: 2, z: 1});
+        assert.closeTo(v1.x, 0.3535, 0.001);
+        assert.closeTo(v1.y, 0.6123, 0.001);
+        assert.closeTo(v1.z, 0.7071, 0.001);
+        assert.closeTo(v2.x, 1.5, 0.001);
+        assert.closeTo(v2.y, 2.5980, 0.001);
+        assert.closeTo(v2.z, 3, 0.001);
+    });
+    it('should rotate in 3D when given centerX, centerY and centerZ', function() {
+        var v1 = {x: 2, y: 3, z: 1};
+        var v2 = {x: -1, y: 2, z: 4};
+        vl.add(v1, v2);
+        vl.rotate(Math.PI/3, Math.PI/4, 2, 2, 1);
+        assert.closeTo(v1.x, 0.3535, 0.001);
+        assert.closeTo(v1.y, 0.6123, 0.001);
+        assert.closeTo(v1.z, 0.7071, 0.001);
+        assert.closeTo(v2.x, 1.5, 0.001);
+        assert.closeTo(v2.y, 2.5980, 0.001);
+        assert.closeTo(v2.z, 3, 0.001);
+    });
+    it('should rotate in 3D when given no center', function() {
+        var v1 = {x: 2, y: 3, z: 1};
+        var v2 = {x: -1, y: 2, z: 4};
+        vl.add(v1, v2);
+        vl.rotate(Math.PI/3, Math.PI/4);
+        assert.closeTo(v1.x, 1.3228, 0.001);
+        assert.closeTo(v1.y, 2.2912, 0.001);
+        assert.closeTo(v1.z, 2.6457, 0.001);
+        assert.closeTo(v2.x, 1.6201, 0.001);
+        assert.closeTo(v2.y, 2.8062, 0.001);
+        assert.closeTo(v2.z, 3.2403, 0.001);
     });
 });
